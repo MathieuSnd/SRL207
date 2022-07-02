5ae933aa2feca4fe0f1124d797d30d99ffe72d0c
Mathieu Serandour <mathieu.serandour@telecom-paris.fr>
2022-07-02 15:07:58 +0200

    wip

--- SimpleClient.java
+++ SimpleClient.java
@@ -218,7 +218,7 @@ public class SimpleClient {
 
                 if(!done)
                     try {
-                        Thread.sleep(20);
+                        Thread.sleep(2000);
                     } catch (InterruptedException e) {
                         // TODO Auto-generated catch block
                         e.printStackTrace();
