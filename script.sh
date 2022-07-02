3e05d4627f4aa5c4edda97c22921a000a3c96111
Mathieu Serandour <mathieu.serandour@telecom-paris.fr>
2022-07-02 16:50:51 +0200

    wip

--- script.sh
+++ script.sh
@@ -3,9 +3,10 @@
 for COMPUTERS in $(seq 1 23); do
 
     rm -f servers.txt
-    head -c $COMPUTERS servers_all.txt > servers.txt
+    head -n $COMPUTERS servers_all.txt > servers.txt
 
-    make > log.txt
-
-    grep Results fetched! < log.txt >> results
+#    make > log.txt
+    cat servers.txt
+    sleep 1
+    #grep Results fetched! < log.txt >> results
 done
